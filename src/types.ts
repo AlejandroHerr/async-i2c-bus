9be5cf159a40dc00dc705bc5495747489a940522
AlejandroHerr <hola@alejandroherr.io>
2018-12-12 14:30:47 +0100

    feat(I2cBus): Add types for I2cBusPromised

---
+++ src/types.ts
@@ -0,0 +1,23 @@
+import { I2cBus, I2cBusFuncs } from 'i2c-bus';
+
+/* tslint:disable-next-line:interface-name */
+export interface I2cBusPromised extends I2cBus {
+  closeAsync: () => Promise<void>;
+
+  i2cFuncsAsync: () => Promise<I2cBusFuncs>;
+  scanAsync: () => Promise<number[]>;
+
+  i2cReadAsync: (address: number, length: number, buffer: Buffer) => Promise<number>;
+  i2cWriteAsync: (address: number, length: number, buffer: Buffer) => Promise<number>;
+
+  receiveByteAsync: (address: number) => Promise<number>;
+  sendByteAsync: (address: number, byte: number) => Promise<void>;
+
+  readByteAsync: (address: number, command: number) => Promise<number>;
+  readWordAsync: (address: number, command: number) => Promise<number>;
+  readI2cBlockAsync: (address: number, command: number, length: number, buffer: Buffer) => Promise<number>;
+
+  writeByteAsync: (address: number, command: number, byte: number) => Promise<void>;
+  writeWordAsync: (address: number, command: number, word: number) => Promise<void>;
+  writeI2cBlockAsync: (address: number, command: number, length: number, buffer: Buffer) => Promise<number>;
+}
