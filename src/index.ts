ea7bb3b4922e7a925bb88284aa21d5eae2209e66
AlejandroHerr <hola@alejandroherr.io>
2018-12-12 14:32:39 +0100

    feat(I2cBus): Add factory for I2cBus mocks

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import { Bus, BusError, BusInterface } from './Bus';
+import createI2cBusMock from './createI2cBusMock';
 
-export { Bus, BusError, BusInterface };
+export { Bus, BusError, BusInterface, createI2cBusMock };
