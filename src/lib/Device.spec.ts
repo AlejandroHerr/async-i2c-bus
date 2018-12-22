8c65770f1b580bf7fc2359b5d461d7cdf3bb3721
AlejandroHerr <hola@alejandroherr.io>
2018-12-22 18:13:40 +0100

    test(Device): Fix test name and remove unnecessary tests

--- src/lib/Device.spec.ts
+++ src/lib/Device.spec.ts
@@ -13,65 +13,53 @@ jest.mock('i2c-bus', () => {
   };
 });
 
-describe('Bus', () => {
-  describe('open', () => {
-    it('should open the bus', async () => {
-      const bus = Bus({ busNumber: 1 });
+describe('Device', () => {
+  it('should call promised async function', async () => {
+    const busNumber = 1;
+    const bus = Bus({ busNumber });
+    const address = 0xab;
+    const device = Device({ address, bus });
 
-      await bus.open();
+    await bus.open();
 
-      expect(bus.isOpen).toBeTruthy();
-      expect(bus.i2cBus).not.toBeNull();
-    });
-  });
-  describe('open', () => {
-    it('should call promised async function', async () => {
-      const busNumber = 1;
-      const bus = Bus({ busNumber });
-      const address = 0xab;
-      const device = Device({ address, bus });
+    const spyOnI2cRead = jest.spyOn(bus, 'i2cRead');
+    await device.i2cRead(1, Buffer.from([]));
+    expect(spyOnI2cRead).toHaveBeenCalledWith(address, 1, Buffer.from([]));
 
-      await bus.open();
+    const spyOnI2cWrite = jest.spyOn(bus, 'i2cWrite');
+    await device.i2cWrite(1, Buffer.from([]));
+    expect(spyOnI2cWrite).toHaveBeenCalledWith(address, 1, Buffer.from([]));
 
-      const spyOnI2cRead = jest.spyOn(bus, 'i2cRead');
-      await device.i2cRead(1, Buffer.from([]));
-      expect(spyOnI2cRead).toHaveBeenCalledWith(address, 1, Buffer.from([]));
+    const spyOnReceiveByte = jest.spyOn(bus, 'receiveByte');
+    await device.receiveByte();
+    expect(spyOnReceiveByte).toHaveBeenCalledWith(address);
 
-      const spyOnI2cWrite = jest.spyOn(bus, 'i2cWrite');
-      await device.i2cWrite(1, Buffer.from([]));
-      expect(spyOnI2cWrite).toHaveBeenCalledWith(address, 1, Buffer.from([]));
+    const spyOnSendByte = jest.spyOn(bus, 'sendByte');
+    await device.sendByte(1);
+    expect(spyOnSendByte).toHaveBeenCalledWith(address, 1);
 
-      const spyOnReceiveByte = jest.spyOn(bus, 'receiveByte');
-      await device.receiveByte();
-      expect(spyOnReceiveByte).toHaveBeenCalledWith(address);
+    const spyOnReadByte = jest.spyOn(bus, 'readByte');
+    await device.readByte(1);
+    expect(spyOnReadByte).toHaveBeenCalledWith(address, 1);
 
-      const spyOnSendByte = jest.spyOn(bus, 'sendByte');
-      await device.sendByte(1);
-      expect(spyOnSendByte).toHaveBeenCalledWith(address, 1);
+    const spyOnReadWord = jest.spyOn(bus, 'readWord');
+    await device.readWord(1);
+    expect(spyOnReadWord).toHaveBeenCalledWith(address, 1);
 
-      const spyOnReadByte = jest.spyOn(bus, 'readByte');
-      await device.readByte(1);
-      expect(spyOnReadByte).toHaveBeenCalledWith(address, 1);
+    const spyOnReadI2cBlock = jest.spyOn(bus, 'readI2cBlock');
+    await device.readI2cBlock(1, 2, Buffer.from([]));
+    expect(spyOnReadI2cBlock).toHaveBeenCalledWith(address, 1, 2, Buffer.from([]));
 
-      const spyOnReadWord = jest.spyOn(bus, 'readWord');
-      await device.readWord(1);
-      expect(spyOnReadWord).toHaveBeenCalledWith(address, 1);
+    const spyOnWriteByte = jest.spyOn(bus, 'writeByte');
+    await device.writeByte(1, 2);
+    expect(spyOnWriteByte).toHaveBeenCalledWith(address, 1, 2);
 
-      const spyOnReadI2cBlock = jest.spyOn(bus, 'readI2cBlock');
-      await device.readI2cBlock(1, 2, Buffer.from([]));
-      expect(spyOnReadI2cBlock).toHaveBeenCalledWith(address, 1, 2, Buffer.from([]));
+    const spyOnWriteWord = jest.spyOn(bus, 'writeWord');
+    await device.writeWord(1, 2);
+    expect(spyOnWriteWord).toHaveBeenCalledWith(address, 1, 2);
 
-      const spyOnWriteByte = jest.spyOn(bus, 'writeByte');
-      await device.writeByte(1, 2);
-      expect(spyOnWriteByte).toHaveBeenCalledWith(address, 1, 2);
-
-      const spyOnWriteWord = jest.spyOn(bus, 'writeWord');
-      await device.writeWord(1, 2);
-      expect(spyOnWriteWord).toHaveBeenCalledWith(address, 1, 2);
-
-      const spyOnWriteI2cBlock = jest.spyOn(bus, 'writeI2cBlock');
-      await device.writeI2cBlock(1, 2, Buffer.from([]));
-      expect(spyOnWriteI2cBlock).toHaveBeenCalledWith(address, 1, 2, Buffer.from([]));
-    });
+    const spyOnWriteI2cBlock = jest.spyOn(bus, 'writeI2cBlock');
+    await device.writeI2cBlock(1, 2, Buffer.from([]));
+    expect(spyOnWriteI2cBlock).toHaveBeenCalledWith(address, 1, 2, Buffer.from([]));
   });
 });
